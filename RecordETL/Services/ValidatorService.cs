412f64bc3465c3a8c17297769695e15624e6c65e
Djelbana <slash.spitfire@gmail.com>
2023-09-26 10:58:25 +0100

    test

--- RecordETL/Services/ValidatorService.cs
+++ RecordETL/Services/ValidatorService.cs
@@ -1,9 +1,5 @@
-﻿using System;
+﻿using RecordETL.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using RecordETL.Models;
 
 namespace RecordETL.Services
 {
