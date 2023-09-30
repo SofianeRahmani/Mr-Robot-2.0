ede99e64070d8fe541be2667871915d0a6fdcbb4
Mohamed Amine BEGHOURA <mohamedamine.beghoura@univ-bba.dz>
2023-09-30 08:25:47 +0100

    Test

--- RecordETL/Services/ExtractorService.cs
+++ RecordETL/Services/ExtractorService.cs
@@ -1,7 +1,10 @@
 ﻿using OfficeOpenXml;
 using RecordETL.Models;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace RecordETL.Services
 {
@@ -32,7 +35,7 @@ namespace RecordETL.Services
             return columns;
         }
 
-        public static RecordSet Extract(string filePath, int pageIndex, List<AttributeIndex> positions)
+        public static RecordSet Extract(string filePath, int pageIndex, List<AttributeIndex> positions, bool isAmerican)
         {
             ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
 
@@ -59,22 +62,22 @@ namespace RecordETL.Services
 
                 var NumeroMembre = positions[0].Index != -1 ? worksheet.Cells[row, positions[0].Index].Text : null;
 
-                var Nom = positions[1].Index != -1 ? worksheet.Cells[row, positions[1].Index+1].Text : null;
-                var Prenom = positions[2].Index != -1 ? worksheet.Cells[row, positions[2].Index+1].Text : null;
-                var Sexe = positions[3].Index != -1 ? worksheet.Cells[row, positions[3].Index+1].Text : null;
-                var CourrielTravail = positions[4].Index != -1 ? worksheet.Cells[row, positions[4].Index+1].Text : null;
+                var Nom = positions[1].Index != -1 ? worksheet.Cells[row, positions[1].Index + 1].Text : null;
+                var Prenom = positions[2].Index != -1 ? worksheet.Cells[row, positions[2].Index + 1].Text : null;
+                var Sexe = positions[3].Index != -1 ? worksheet.Cells[row, positions[3].Index + 1].Text : null;
+                var CourrielTravail = positions[4].Index != -1 ? worksheet.Cells[row, positions[4].Index + 1].Text : null;
                 var CourrielPersonnel = positions[5].Index != -1 ? worksheet.Cells[row, positions[5].Index + 1].Text : null;
-                var CourrielAutre = positions[6].Index != -1 ? worksheet.Cells[row, positions[6].Index+1].Text : null;
-                var Telephone = positions[7].Index != -1 ? worksheet.Cells[row, positions[7].Index +1].Text : null;
-                var TelephoneTravail = positions[8].Index != -1 ? worksheet.Cells[row, positions[8].Index+1].Text : null;
+                var CourrielAutre = positions[6].Index != -1 ? worksheet.Cells[row, positions[6].Index + 1].Text : null;
+                var Telephone = positions[7].Index != -1 ? worksheet.Cells[row, positions[7].Index + 1].Text : null;
+                var TelephoneTravail = positions[8].Index != -1 ? worksheet.Cells[row, positions[8].Index + 1].Text : null;
                 var TelephoneCellulaire = positions[9].Index != -1 ? worksheet.Cells[row, positions[9].Index + 1].Text : null;
-                var Adresse = positions[10].Index != -1 ? worksheet.Cells[row, positions[10].Index +1].Text : null;
+                var Adresse = positions[10].Index != -1 ? worksheet.Cells[row, positions[10].Index + 1].Text : null;
                 var Ville = positions[11].Index != -1 ? worksheet.Cells[row, positions[11].Index + 1].Text : null;
                 var Province = positions[12].Index != -1 ? worksheet.Cells[row, positions[12].Index + 1].Text : null;
-                var CodePostal = positions[13].Index != -1 ? worksheet.Cells[row, positions[13].Index +1].Text : null;
-                var Nas = positions[14].Index != -1 ? worksheet.Cells[row, positions[14].Index +1].Text : null;
+                var CodePostal = positions[13].Index != -1 ? worksheet.Cells[row, positions[13].Index + 1].Text : null;
+                var Nas = positions[14].Index != -1 ? worksheet.Cells[row, positions[14].Index + 1].Text : null;
                 var Categories = positions[15].Index != -1 ? worksheet.Cells[row, positions[15].Index + 1].Text : null;
-                var DateNaissance = positions[16].Index != -1 ? worksheet.Cells[row, positions[16].Index +1].Text : null;
+                var DateNaissance = positions[16].Index != -1 ? worksheet.Cells[row, positions[16].Index + 1].Text : null;
                 var DateAnciennete = positions[17].Index != -1 ? worksheet.Cells[row, positions[17].Index + 1].Text : null;
                 var Anciennete = positions[18].Index != -1 ? worksheet.Cells[row, positions[18].Index + 1].Text : null;
                 var DateEmbauche = positions[19].Index != -1 ? worksheet.Cells[row, positions[19].Index + 1].Text : null;
@@ -82,8 +85,8 @@ namespace RecordETL.Services
                 var DateStatut = positions[21].Index != -1 ? worksheet.Cells[row, positions[21].Index + 1].Text : null;
                 var IdSystemeSource = positions[22].Index != -1 ? worksheet.Cells[row, positions[22].Index + 1].Text : null;
                 var Secteur = positions[23].Index != -1 ? worksheet.Cells[row, positions[23].Index + 1].Text : null;
-                var StatutPersonne = positions[24].Index != -1 ? worksheet.Cells[row, positions[24].Index +1].Text : null;
-                var IdentifiantAlternatif = positions[25].Index != -1 ? worksheet.Cells[row, positions[25].Index +1].Text : null;
+                var StatutPersonne = positions[24].Index != -1 ? worksheet.Cells[row, positions[24].Index + 1].Text : null;
+                var IdentifiantAlternatif = positions[25].Index != -1 ? worksheet.Cells[row, positions[25].Index + 1].Text : null;
                 var InfosComplementaires1 = positions[26].Index != -1 ? worksheet.Cells[row, positions[26].Index + 1].Text : null;
                 var InfosComplementaires2 = positions[27].Index != -1 ? worksheet.Cells[row, positions[27].Index + 1].Text : null;
 
@@ -100,19 +103,19 @@ namespace RecordETL.Services
                 {
                     row = row,
                     NumeroMembre = NumeroMembre,
-                    Nom = Nom,
-                    Prenom = Prenom,
+                    Nom = Nom.TrimEnd(),
+                    Prenom = Prenom.TrimEnd(),
                     Sexe = Sexe,
                     CourrielTravail = CourrielTravail,
                     CourrielPersonnel = CourrielPersonnel,
                     CourrielAutre = CourrielAutre,
-                    Telephone = Telephone,
-                    TelephoneTravail = TelephoneTravail,
-                    TelephoneCellulaire = TelephoneCellulaire,
+                    Telephone = FormatPhoneNumber(Telephone),
+                    TelephoneTravail = FormatPhoneNumber(TelephoneTravail),
+                    TelephoneCellulaire = FormatPhoneNumber(TelephoneCellulaire),
                     Adresse = Adresse,
                     Ville = Ville,
                     Province = Province,
-                    CodePostal = CodePostal,
+                    CodePostal = FormatPostalCode(CodePostal, isAmerican),
                     Nas = Nas,
                     Categories = Categories,
                     DateNaissance = DateNaissance,
@@ -142,6 +145,45 @@ namespace RecordETL.Services
 
 
             return recordSet;
+        }
+
+
+        static string FormatPhoneNumber(string phoneNumber)
+        {
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                return phoneNumber;
+            }
+
+            phoneNumber = Regex.Replace(phoneNumber, @"\D", "");
+            if (phoneNumber.Any(c => !char.IsDigit(c)))
+            {
+                return phoneNumber;
+            }
+
+            return $"{phoneNumber.Substring(0, 3)}-{phoneNumber.Substring(3, 3)}-{phoneNumber.Substring(6, 4)}";
+        }
+
+
+        static string FormatPostalCode(string postalCode, bool isAmerican)
+        {
+            // Suppression de tous les caractères non alphanumériques
+            postalCode = Regex.Replace(postalCode, @"[^\w]", "");
+
+            if (isAmerican)
+            {
+                if (postalCode.Length != 5 || !Regex.IsMatch(postalCode, @"^\d{5}$"))
+                    throw new ArgumentException("Le code postal américain doit être composé de 5 chiffres.");
+
+                return postalCode;
+            }
+            else
+            {
+                if (postalCode.Length != 6 || !Regex.IsMatch(postalCode, @"^[A-Za-z]\d[A-Za-z]\d[A-Za-z]\d$"))
+                    throw new ArgumentException("Le code postal canadien doit être composé de 6 caractères alphanumériques alternés.");
+
+                return $"{postalCode.Substring(0, 3)} {postalCode.Substring(3, 3)}";
+            }
         }
     }
 }
