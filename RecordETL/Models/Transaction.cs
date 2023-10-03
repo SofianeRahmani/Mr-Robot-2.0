a27531ad5ecfd4e6f7be4a28eec568d2463a1a27
Mohamed Amine BEGHOURA <mohamedamine.beghoura@univ-bba.dz>
2023-10-03 17:23:16 +0100

    Test

---
+++ RecordETL/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace RecordETL.Models
+{
+    public class Transaction
+    {
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+        public string DepositDate { get; set; }
+        public string Amount { get; set; }
+        public string Type { get; set; }
+        public string Account { get; set; }
+        public string Note { get; set; }
+    }
+}
